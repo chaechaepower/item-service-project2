728e2b6c6cbc0963693b57adb8788f1aa5e22f1d
Chaeyoon Kim <jkcyforever@gmail.com>
2023-10-14 17:34:47 +0900

    complete login service

--- src/main/java/chae/onlinestore/web/HomeController.java
+++ src/main/java/chae/onlinestore/web/HomeController.java
@@ -1,12 +1,31 @@
 package chae.onlinestore.web;
 
+import chae.onlinestore.domain.member.Member;
+import chae.onlinestore.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
+
+    private final MemberRepository memberRepository;
+
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String homeLogin(@SessionAttribute(name=SessionConst.LOGIN_MEMBER, required=false)
+            Member loginMember, Model model) {
+
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
---
+++ src/main/java/chae/onlinestore/web/SessionConst.java
@@ -0,0 +1,5 @@
+package chae.onlinestore.web;
+
+public class SessionConst {
+    public static final String LOGIN_MEMBER="loginMember";
+}
